662a6ed40b859c5b8903de2227644f8b1f32ba47
陈宇峰 <yufengcode@gmail.com>
2016-10-06 21:04:57 +0800

    init

--- 34. Search for a Range/main.cpp
+++ 34. Search for a Range/main.cpp
@@ -7,9 +7,91 @@
 //
 
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+/**
+ *  Given [5, 7, 7, 8, 8, 10] and target value 8,
+     return [3, 4].
+ *   
+ 使用二分查找
+ */
+
+long BinarySearch(vector<int>& nums, long begin, long end, int target)
+{
+    while (begin <= end)
+    {
+        long mid = (begin + end) / 2;
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+        else if (target < nums[mid])
+        {
+            end = mid - 1;
+        }
+        else if (target > nums[mid])
+        {
+            begin = mid + 1;
+        }
+    }
+
+    return -1;
+}
+
+vector<int> searchRange(vector<int>& nums, int target)
+{
+    long begin = 0;
+    long end = nums.size() - 1;
+
+    long index = BinarySearch(nums, begin, end, target);
+
+    vector<int> result;
+    if (index == -1)
+    {
+        result.push_back(-1);
+        result.push_back(-1);
+    }
+    else
+    {
+        // 继续进行二分查找
+        long left = BinarySearch(nums, begin, index - 1, target);
+        long right = BinarySearch(nums, index + 1, end, target);
+
+        if (left == -1)
+        {
+            result.push_back((int)index);
+        }
+        else
+        {
+            result.push_back((int)left);
+        }
+
+        if (right == -1)
+        {
+            result.push_back((int)index);
+        }
+        else
+        {
+            result.push_back((int)right);
+        }
+    }
+
+    return result;
+}
+
+int main(int argc, const char * argv[])
+{
+    int arr[] = {5, 7, 7, 8, 8, 10};
+    vector<int>vv(arr,arr+sizeof(arr)/sizeof(int));
+
+    vector<int> res = searchRange(vv, 7);
+
     return 0;
 }
+
+
+
+
+
